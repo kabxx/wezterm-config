47cc32246af34d659ae140f589101e95311a6049
wxh <2201174299@qq.com>
2026-07-29 01:48:54 +0800

    chore: customize wezterm for local setup

--- config/appearance.lua
+++ config/appearance.lua
@@ -4,7 +4,7 @@ local colors = require('colors.custom')
 
 ---@type Config
 return {
-   max_fps = 120,
+   max_fps = 60,
    front_end = 'WebGpu', ---@type 'WebGpu' | 'OpenGL' | 'Software'
    webgpu_power_preference = 'HighPerformance',
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
@@ -13,7 +13,7 @@ return {
    underline_thickness = '1.5pt',
 
    -- cursor
-   animation_fps = 120,
+   animation_fps = 60,
    cursor_blink_ease_in = 'EaseOut',
    cursor_blink_ease_out = 'EaseOut',
    default_cursor_style = 'BlinkingBlock',
--- config/domains.lua
+++ config/domains.lua
@@ -13,30 +13,12 @@ local options = {
 }
 
 if platform.is_win then
-   options.ssh_domains = {
-      {
-         name = 'ssh:wsl',
-         username = 'kevin',
-         remote_address = 'localhost',
-         multiplexing = 'None',
-         default_prog = { 'fish', '-l' },
-         assume_shell = 'Posix',
-      },
-   }
-
    options.wsl_domains = {
       {
-         name = 'wsl:ubuntu-fish',
+         name = 'wsl:ubuntu',
          distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
-      },
-      {
-         name = 'wsl:ubuntu-bash',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
+         username = 'wxh',
+         default_cwd = '/home/wxh',
          default_prog = { 'bash', '-l' },
       },
    }
