35e0a9fe929f21257670d31260592b80fddf1428
wxh <2201174299@qq.com>
2026-09-21 18:19:00 +0800

    chore(wezterm): 从启动菜单移除 Fish
    Linux/Mac 启动菜单不再列出 Fish，默认仍是 zsh。

--- config/launch.lua
+++ config/launch.lua
@@ -17,17 +17,17 @@ elseif platform.is_mac then
    options.default_prog = { '/bin/zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
+
 elseif platform.is_linux then
    options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
+
 end
 
 return options
